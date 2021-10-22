8939c079bcab5b023c4ee732cab8b2f929435ae4
Max Kim <hwaseen@gmail.com>
2021-10-22 15:30:56 +0900

    5개 페이지에 대한 기본 URL, URL 파라미터 세팅 완료

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_start, name='start'),
+    path('', views.get_start_page, name='start'),
+    path('generate', views.get_generate_page, name='generate'),
+    path('generate/<int:quiz_set_id>', views.get_generate_result_page, name='generate-result'),
+    path('solve/<int:quiz_set_id>', views.get_solve_page, name='solve'),
+    path('result/<int:quiz_set_id>/<int:result_id>', views.get_result_page, name='result'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def get_start(request):
-    return render(request, 'quiz/start.html', {})
+def get_start_page(request):
+    return render(request, 'quiz/startPage.html', {})
+
+def get_generate_page(request):
+    return render(request, 'quiz/generatePage.html', {})
+
+def get_generate_result_page(request, quiz_set_id):
+    return render(request, 'quiz/generateResultPage.html', {'quiz_set_id':quiz_set_id})
+
+def get_solve_page(request, quiz_set_id):
+    return render(request, 'quiz/solvePage.html', {'quiz_set_id':quiz_set_id})
+
+def get_result_page(request, quiz_set_id, result_id):
+    return render(request, 'quiz/resultPage.html', {'quiz_set_id':quiz_set_id,'result_id':result_id})
