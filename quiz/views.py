8e15e66baaaf5fdce4b2186a54b9737702a89b36
Max Kim <hwaseen@gmail.com>
2021-11-06 21:12:13 +0900

    동적 공유 URL 설정

--- quiz/views.py
+++ quiz/views.py
@@ -10,6 +10,7 @@ from django.contrib import messages
 def get_start_page(request):
     return render(request, 'quiz/startPage.html', {})
 
+
 def get_generate_page(request):
     if request.method == 'POST':
         quiz_set_form = QuizSetForm(request.POST)
@@ -35,15 +36,21 @@ def get_generate_page(request):
         quiz_set_form = QuizSetForm()
         quiz_form_set = QuizFormSet(queryset=Quiz.objects.none())
         return render(request, 'quiz/generatePage.html', {
-            'quiz_set_form':quiz_set_form,
+            'quiz_set_form': quiz_set_form,
             'quiz_form_set': quiz_form_set
         })
 
+
 def get_generate_result_page(request, quiz_set_id):
-    return render(request, 'quiz/generateResultPage.html', {'quiz_set_id':quiz_set_id})
+    return render(request, 'quiz/generateResultPage.html', {
+        'quiz_set_id': quiz_set_id,
+    })
+
 
 def get_solve_page(request, quiz_set_id):
     quizes = Quiz.objects.filter(quiz_set_id = quiz_set_id)
     return render(request, 'quiz/solvePage.html', {'quiz_set_id':quiz_set_id,'quizes':quizes})
+
+
 def get_result_page(request, quiz_set_id, result_id):
     return render(request, 'quiz/resultPage.html', {'quiz_set_id':quiz_set_id,'result_id':result_id})
