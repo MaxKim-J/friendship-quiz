2c8980927b45a11b75c2ddcf51c49a5ca845ebf9
Max Kim <hwaseen@gmail.com>
2021-11-13 17:43:34 +0900

    Debug True로 유지

--- friendshipquiz/settings.py
+++ friendshipquiz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sf)e1!h0lk*)=7#^)1$39oii)ja+-l@@brn)%*o5^bq9qsn5zk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'friendship-quiz-live.herokuapp.com']
 
